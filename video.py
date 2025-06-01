92f5f46b567522d301a75c98624c0678b58b9603
Gustavo Henrique de Almeida <gu.henriqueamorim@gmail.com>
2025-06-01 14:22:06 -0300

    Codigo
    Codigo em python para rodar o video da maneira correta

---
+++ video.py
@@ -0,0 +1,37 @@
+from ultralytics import YOLO
+import cv2
+
+# Carrega o modelo
+model = YOLO("runs/detect/cigarrovapedetect/weights/best.pt")
+
+# Caminho do vídeo
+video_path = "C:/Python/meuvideo.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Verifica se abriu corretamente
+if not cap.isOpened():
+    print("Erro ao abrir o vídeo.")
+    exit()
+
+# Configura para salvar o vídeo com anotações
+width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps    = cap.get(cv2.CAP_PROP_FPS)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec
+out = cv2.VideoWriter('saida_detectada.mp4', fourcc, fps, (width, height))
+
+# Loop pelos frames
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model.predict(source=frame, imgsz=640, conf=0.3, verbose=False)
+    annotated_frame = results[0].plot()
+    out.write(annotated_frame)
+
+# Libera os recursos
+cap.release()
+out.release()
+print("Processamento concluído. Vídeo salvo como 'saidasalva.mp4'")
